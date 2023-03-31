b3f588473a2b858abd5252636952d37cdbeb8339
arsenykruglikov <kruglikov.arseny@gmail.com>
2023-03-31 12:30:21 +0300

    feat: changed redirects regExp

--- config-helpers/redirects/common.js
+++ config-helpers/redirects/common.js
@@ -14,17 +14,8 @@ function getRedirect(source, destination, userAgentRegexp) {
 }
 
 const uaListByPlatform = {
-    sberbox: [
-        'sberbox; SberBox',
-        'satellite1; SberBox',
-        'satellite1; SMART BOX',
-        'satellite; Satellite',
-        'tv; TV',
-        'satellite; SberBox Top',
-        'huawei-tv; Huawei TV',
-        'sberboxtime; SberBox Time',
-    ],
-    portal: ['stargate; SberPortal', 'stargate'],
+    sberbox: ['sberbox', 'satellite', 'tv'],
+    portal: ['sberportal', 'stargate'],
     mobile: ['.*'],
 };
 
