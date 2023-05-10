831779c24321c562b08b74a942a754c0ddcf5808
Troff8 <troffibp@gmail.com>
2023-05-10 13:14:05 +0400

    (SPEED-149): убрал лишние css свойства

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -5,10 +5,9 @@ const StyledContainer = styled(Container)`
     align-items: center;
     flex: 1;
     margin-top: 10rem;
+
     @media (max-width: 768px) {
-        margin-top: 5rem;
         text-align: center;
-        padding: 2rem;
     }
 `;
 
