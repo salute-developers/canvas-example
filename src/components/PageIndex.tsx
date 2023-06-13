84cdb1a5cbcd3ec830c0eff7da53fc4aea2e213e
KateKate <anishkina@gmail.com>
2023-06-13 10:56:16 +0300

    SPEED-987: spatial for PageIndex, refactoring

--- src/components/PageIndex.tsx
+++ src/components/PageIndex.tsx
@@ -2,8 +2,6 @@ import { NeuHeader } from '@salutejs/plasma-ui';
 
 import { NoteList } from './NoteList';
 
-export { NoteList } from './NoteList';
-
 export const PageIndex = () => (
     <>
         <NeuHeader arrow="minimize" />
